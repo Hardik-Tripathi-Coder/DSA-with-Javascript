720648f499c0894500becc26c78f1e325bb0cba0
Hardik-Tripathi-Coder <hardik.tripathi.0001@gmail.com>
2026-09-11 18:06:22 +0530

    Conditional Statement

---
+++ conditional.js
@@ -0,0 +1,45 @@
+// Conditional Statements
+
+// Ques 1 : Valid Voter
+
+// let voterAge = Number(prompt("Enter your Age"));
+
+// if(isNaN(voterAge)){
+//     console.log("Incorrect Input Detected !! ");
+// }
+
+// else if(voterAge >= 18){
+//     console.log("You can Vote.");
+// }
+
+// else {
+//     console.log("You cannot Vote");
+// }
+
+// Ques 2 : Shop Discount
+
+// Discount Formula : Discount * Amount / 100 ;
+
+// let payAmount = Number(prompt("Enter Your Bill Amount"));
+// let discount = 0 ;
+
+
+// if(isNaN(payAmount)){
+//     console.log("Ouch !! Don't do this .");
+// }
+// else if(payAmount > 0 && payAmount <= 5000){
+//     discount = 0 ;
+// }
+// else if(payAmount <= 7000 ){
+//     discount = 5 ;
+// }
+// else if(payAmount <= 9000 ){
+//     discount = 10 ;
+// }
+// else{
+//     discount = 20 ;
+// }
+// let pay = payAmount - Math.floor((discount*payAmount)/100) ;
+// console.log(pay); efficient way to calculate the final amount after discount.
+
+
